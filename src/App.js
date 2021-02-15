938982e9293c428515d37ec81ee0d742cbeb9a21
oliviarpo <oliviapovey@outlook.com>
2021-02-15 11:58:32 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/oliviapovey/">Olivia Povey</a> and
-        is open-sourced on{" "}
-        <a
-          href="https://github.com/oliviarpo/react-final-project"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div class="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/oliviapovey/">Olivia Povey</a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/oliviarpo/react-final-project"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
